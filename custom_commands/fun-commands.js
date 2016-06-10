da9b7e699a34722cdd6aa8dba2c09bbe7c80e28c
unknown <mpayne01001@gmail.com>
2016-06-10 18:40:42 -0400

    Messing around

--- custom_commands/fun-commands.js
+++ custom_commands/fun-commands.js
@@ -41,7 +41,31 @@ function cmdFlipCoin(request){
 }
 
 function cmdRollDice(request){
-  
+  var regex = /^\/roll (\d+)d(\d+)/i;
+
+  if (regex.test(request.text)) {
+    var val = regex.exec(request.text);
+    var msg = "";
+
+    if (val[1] < 1) {
+      msg = "Hey I rolled no dice and the result is ... You're an idiot";
+    } else if (val[1] > 20) {
+      msg = "If I can't fit more than 20 dice in my robot hand";
+    } else if (val[2] > 1000) {
+      msg = "I'm 3d printing dice with more than 1000 sides just for this silly request. I'll get back to you in 37 years.";
+    } else if (val[2] < 2) {
+      msg = "Your results are: Unicorns";
+    } else {
+      for (i = 0; i < val[1]; i++) {
+        var die = Math.floor((Math.random() * val[2]) + 1);
+        msg += " " + die;
+      }
+      msg = "Your results are:" + msg;
+    }
+    return msg;
+  } else {
+    return false;
+  }
 }
 
 function cmdRPS(request){
